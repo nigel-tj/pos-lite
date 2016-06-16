9111ff04315aa47a39f169c9ef99b10c1adfeab4
tokhi <shafi.tokhi@gmail.com>
2016-06-16 17:46:03 +0200

    auto complete  minor refactoring

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -95,14 +95,14 @@ $(document).ready(function(){
 	});
 
 	// creates a line item for a sale
-	var input = $('#search_item_name')[0]
-	var sale_id = parseInt($('#search_sale_id')[0].value)
+	var input = $('#search_item_name')[0];
+	var sale_id = $('#search_sale_id')[0];
 	  Awesomplete.$.bind(input, {
 		  "awesomplete-selectcomplete": function(evt) {		  	
 		   $.ajax({
 		      type: "GET",
 		      url: '/sales/create_line_item', //sumbits it to the given url of the form
-		      data: { item_id: parseInt(input.value.split(".|")[0]), quantity: 1, sale_id: sale_id},
+		      data: { item_id: parseInt(input.value.split(".|")[0]), quantity: 1, sale_id: parseInt(sale_id.value)},
 		      dataType: "script",
 		      success: function() {
 		      	// prepare the search box for the new entry
